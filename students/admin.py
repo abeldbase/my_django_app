730833a78f9f822f0ca6feee1f5ee464ded1df4f
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2023-03-09 20:28:54 -0800

    creating detail and all html file , creating admin and extra user

--- students/admin.py
+++ students/admin.py
@@ -8,6 +8,10 @@ from students.models import Student
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['lastname', 'firstname', 'date_of_birth', 'phone',
                     'city', 'zip', 'state', 'graduation', 'email', 'year']
+    list_filter = ['lastname', 'firstname', 'date_of_birth', 'phone',
+                   'city', 'zip', 'state', 'graduation', 'email', 'year']
+    search_fields = ['lastname', 'firstname', 'date_of_birth', 'phone',
+                     'city', 'zip', 'state', 'graduation', 'email', 'year']
 
 
 admin.site.register(Student, StudentAdmin)
